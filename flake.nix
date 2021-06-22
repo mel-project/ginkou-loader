f3eeaa1cd2da8916b7b89a855cc7cb820c60991b
Jay Butera <buterajay@protonmail.com>
2021-06-22 15:47:22 -0500

    Remove bundling logic, clean up

--- flake.nix
+++ flake.nix
@@ -5,9 +5,6 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.melwalletd-flake.url = "github:themeliolabs/melwalletd";
   inputs.mozilla = { url = "github:mozilla/nixpkgs-mozilla"; flake = false; };
-  #inputs.ginkou-src = { url = "github:themeliolabs/ginkou"; flake = false; };
-  inputs.ginkou-src = { url = "/home/casper/Programming/themelio/ginkou"; flake = false; };
-  #inputs.ginkou-loader-src = { url = "github:themeliolabs/ginkou-loader"; flake = false; };
 
   outputs =
     { self
@@ -15,8 +12,6 @@
     , mozilla
     , flake-utils
     , melwalletd-flake
-    #, ginkou-loader-src
-    , ginkou-src
     , ...
     } @inputs:
     let
@@ -33,8 +28,8 @@
           cargo = rustChannel.rust;
         };
 
-    in flake-utils.lib.eachSystem
-      ["x86_64-linux"]
+    in flake-utils.lib.eachDefaultSystem
+      #["x86_64-linux"]
       (system: let
 
         pkgs = import nixpkgs {
@@ -58,12 +53,6 @@
               (rustChannel.rust.override { extensions = [ "rust-src" ]; })
               binutils
               zlib
-              #wget
-              #curl
-              #openssl
-              #squashfsTools
-              #libsoup
-
               glib
               libappindicator-gtk3
               webkit
@@ -83,53 +72,10 @@
           ];
         };
 
-        ginkou = pkgs.callPackage "${ginkou-src}/rollup-build.nix" {
-          nodejs = pkgs.nodejs-12_x;
-          rollup = pkgs.nodePackages.rollup;
-        };
-
-        melwalletd = melwalletd-flake.packages."${system}".melwalletd;
-
-        bundle = pkgs.callPackage ./bundle.nix {
-          inherit melwalletd ginkou ginkou-loader;
-        };
-
         in rec {
           packages.ginkou-loader = ginkou-loader;
 
           # Produces ginkou binary and melwalletd linked binary
-          defaultPackage = bundle;
-
-          /*
-          devShell = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              docker
-              #packages.tauri
-            ] ++ tauri-deps;
-
-            shellHook = ''
-              export OPENSSL_DIR="${pkgs.openssl.dev}"
-              export OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib"
-
-              # melwalletd
-              export PATH=$PATH:${melwalletd}/bin
-
-              # Copy in ginkou repo
-              cp -r ${ginkou} ./ginkou
-
-              # Make writable for building
-              chmod +w ginkou
-              chmod +w ginkou/public
-              mkdir ginkou/public/build
-
-              # Place into project with target triplet for bundling
-              cp ${melwalletd}/bin/melwalletd ./src-tauri/melwalletd-$(gcc -dumpmachine)
-
-              # Tauri cli
-              export PATH=$PATH:${packages.tauri}/bin
-              alias tauri='cargo-tauri tauri'
-            '';
-          };
-          */
+          defaultPackage = ginkou-loader;
         });
 }
