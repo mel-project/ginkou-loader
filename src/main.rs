afc5d0d5ef100e697660edaef40095118c313833
nullchinchilla <nullchinchilla@pm.me>
2021-07-21 16:55:19 -0500

    work around CORS by just spinning up a localhost http server

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{path::PathBuf, process::Command};
 
 use anyhow::Context;
 use argh::FromArgs;
+use tide::listener::Listener;
 use wry::{
     application::{
         event::{Event, StartCause, WindowEvent},
@@ -28,6 +29,22 @@ fn main() -> anyhow::Result<()> {
 
     let args: Args = argh::from_env();
 
+    // first, we start a tide-based server that runs off serving the directory
+    let html_path = args.html_path.clone();
+    let (send_addr, recv_addr) = smol::channel::unbounded();
+    smol::spawn(async move {
+        let mut app = tide::new();
+        app.at("/").serve_dir(html_path).unwrap();
+        let mut listener = app.bind("127.0.0.1:0").await.unwrap();
+        send_addr
+            .send(listener.info()[0].connection().to_string())
+            .await
+            .unwrap();
+        listener.accept().await.unwrap()
+    })
+    .detach();
+    let html_addr = smol::future::block_on(recv_addr.recv())?;
+
     // first start melwalletd
     // TODO: start melwalletd with proper options, especially authentication!
     let mut cmd = Command::new(args.melwalletd_path.as_os_str())
@@ -35,21 +52,20 @@ fn main() -> anyhow::Result<()> {
         .arg(wallets_dir.as_os_str())
         .spawn()
         .context("cannot spawn melwalletd")?;
-    scopeguard::defer!(cmd.kill().unwrap());
 
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("Hello World")
         .build(&event_loop)?;
     let _webview = WebViewBuilder::new(window)?
-        .with_custom_protocol("wry".to_string(), move |_, url| {
-            let url = url.replace("wry:///", "");
-            let mut path = args.html_path.clone();
-            path.push(url);
-            dbg!(&path);
-            Ok(std::fs::read(&path)?)
-        })
-        .with_url("wry:///index.html")?
+        // .with_custom_protocol("wry".to_string(), move |_, url| {
+        //     let url = url.replace("wry://localhost/", "");
+        //     let mut path = args.html_path.clone();
+        //     path.push(url);
+        //     dbg!(&path);
+        //     Ok(std::fs::read(&path)?)
+        // })
+        .with_url(&format!("{}/index.html", html_addr))?
         .build()?;
 
     event_loop.run(move |event, _, control_flow| {
@@ -60,7 +76,10 @@ fn main() -> anyhow::Result<()> {
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 ..
-            } => *control_flow = ControlFlow::Exit,
+            } => {
+                scopeguard::defer!(cmd.kill().unwrap());
+                *control_flow = ControlFlow::Exit
+            }
             _ => (),
         }
     });
